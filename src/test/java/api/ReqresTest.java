b22f0d68f8e5ecd937de9185f2a4bb95cb7d514c
Kononirin <kononirin@gmail.com>
2023-09-17 22:13:43 +0500

    Add class Specification

--- src/test/java/api/ReqresTest.java
+++ src/test/java/api/ReqresTest.java
@@ -13,10 +13,10 @@ public class ReqresTest {
 
     @Test
     public void checkAvatarAndIdTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecError400());
         List<UserData> users = given()
                 .when()
-                .contentType(ContentType.JSON)
-                .get(URL + "api/users?page=2")
+                .get("api/users?page=2")
                 .then().log().all()
                 .extract().body().jsonPath().getList("data", UserData.class);
 
@@ -31,6 +31,11 @@ public class ReqresTest {
         for(int i = 0; i < avatars.size(); i++) {
             Assert.assertTrue(avatars.get(i).contains(ids.get(i)));
         }
+    }
+
+    @Test
+    public void succesRegTest() {
+
     }
 
 
